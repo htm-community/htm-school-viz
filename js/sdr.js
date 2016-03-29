4e485906ce2a6eed6d760a953e84a1706a262a43
Matthew Taylor <matt@numenta.org>
2016-03-29 09:32:46 -0700

    Added new comparison view.

--- js/sdr.js
+++ js/sdr.js
@@ -50,6 +50,44 @@ $(function() {
         drawUnion: function(left, right, selector, title) {
             var union = SDR.tools.union(left, right);
             this.draw(union, selector, title);
+        },
+
+        drawComparison: function(left, right, selector, title) {
+            var rowLength = Math.floor(Math.sqrt(left.length));
+            d3.select(selector)
+                .selectAll("rect")
+                .data(d3.range(0, left.length))
+                .enter()
+                .append("rect")
+                .attr("x", function(i) {
+                    var offset = i % rowLength;
+                    return offset * POINT_SIZE;
+                })
+                .attr("y", function(i) {
+                    var offset = Math.floor(i / rowLength);
+                    return offset * POINT_SIZE;
+                })
+                .attr("width", POINT_SIZE)
+                .attr("height", POINT_SIZE)
+                .style("fill", function(i) {
+                    var leftBit = left[i];
+                    var rightBit = right[i];
+                    if (leftBit == 1 && rightBit == 1) {
+                        return "red";
+                    } else if (leftBit == 1 && rightBit == 0) {
+                        return "orange";
+                    } else if (leftBit == 0 && rightBit == 1) {
+                        return "green";
+                    }
+                    return "white";
+                });
+
+            if (title) {
+                $(selector).before('<h3>' + title + '</h3>');
+            }
+            $(selector).before('<p>RED: overlap</p>');
+            $(selector).before('<p>Orange: left bits</p>');
+            $(selector).before('<p>Green: right bits</p>');
         }
     };
 
