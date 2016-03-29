0dbe0bf75b274d43e585b5bb058fc9dfae53e906
Matthew Taylor <matt@numenta.org>
2016-03-29 08:13:53 -0700

    Working on adding noise.

--- js/tools.js
+++ js/tools.js
@@ -6,6 +6,11 @@ $(function() {
         return Math.floor(Math.random() * (max - min)) + min;
     }
 
+    function flip(bit) {
+        if (bit == 0) return 1;
+        return 0;
+    }
+
     window.SDR.tools = {
         getRandom: function(size, sparsity) {
             var out = [];
@@ -28,6 +33,19 @@ $(function() {
             }
 
             return out;
+        },
+
+        // I am unsure whether this is the right way to add noise to an SDR.
+        addNoise: function(sdr, percentNoise) {
+            var noisy = [];
+            _.each(sdr, function(bit) {
+                var newBit = bit;
+                if (Math.random() <= percentNoise) {
+                    newBit = flip(bit);
+                }
+                noisy.push(newBit);
+            });
+            return noisy;
         },
 
         population: function(sdr) {
