096783b8dea9e7501e07dd2e636def8ce7df9e66
Matthew Taylor <matt@numenta.org>
2016-05-09 11:41:36 -0700

    Better sorted sdr matching

--- js/classification.js
+++ js/classification.js
@@ -5,6 +5,8 @@ $(function() {
     // Primitives
     var n = 256;
     var w = 4;
+    //var n = 2048;
+    //var w = 40;
     var sparsity = w / n;
     var theta = Math.floor(w * (3/4));
     var t = w - theta;
@@ -48,6 +50,10 @@ $(function() {
         nextSdr = SDR.tools.getRandom(n, w);
     }
 
+    function setTheta(myTheta) {
+        theta = myTheta;
+    }
+
     /* UI and Draw functions */
 
     function drawSliders() {
@@ -67,6 +73,10 @@ $(function() {
             min: 1, max: w, value: theta, step: 1,
             disabled: true,
             slide: function(event, ui) {
+                if (validate(w, ui.value, t)) {
+                    setTheta(ui.value);
+                    matchStack();
+                }
                 //drawMatch(ui.value, t, function(err) {
                 //    if (err) event.preventDefault();
                 //});
@@ -166,7 +176,7 @@ $(function() {
         });
         $populateBtn.click(function() {
             sdrStack = sdrStack.concat(_.map(_.range(50), function() {
-                return SDR.tools.getRandom(n, _.random(3, 14));
+                return SDR.tools.getRandom(n, _.random(w-1, w+1));
             }));
             drawSdrStack();
         });
@@ -199,7 +209,7 @@ $(function() {
                 } else {
                     matchSdr[index] = bit;
                     drawSdrStack();
-                    matchStack(matchSdr);
+                    matchStack();
                     drawMatchSdr();
                 }
             }
@@ -221,7 +231,7 @@ $(function() {
                 //    t = Math.floor((matchW - theta) * (1/2));
                 //}
                 drawSdrStack();
-                matchStack(matchSdr);
+                matchStack();
                 $nextSdr.addClass('highlight');
                 $sdrSvg.parent().addClass('highlight');
                 drawMatchSdr();
@@ -253,31 +263,39 @@ $(function() {
             $populateBtn.prop('disabled', false);
             drawNextSdr();
         } else {
+            $switchBtn.prop('disabled', true);
             $wSlider.slider('option', 'disabled', true);
             $thetaSlider.slider('option', 'disabled', false);
             $tSlider.slider('option', 'disabled', false);
             $addBtn.prop('disabled', true);
             $populateBtn.prop('disabled', true);
             $nextSdr.html('');
+            $('.btn-group').slideUp();
+            $('.buttons hr').slideDown();
         }
     }
 
     function getUiMatchIndication(sdr, matchSdr) {
         var overlap = SDR.tools.getOverlapScore(sdr, matchSdr);
-        var percent = Math.floor(overlap / theta * 100);
+        var percent = Math.min(Math.floor(overlap / theta * 100), 100);
         var clazz = '';
         if (overlap >= theta) clazz = 'match';
-        var html = '<div class="progress ' + clazz + '">'
+        return '<div class="progress ' + clazz + '">'
             + '<div class="progress-bar" role="progressbar" '
             + 'aria-valuenow="' + percent + '" aria-valuemin="0" aria-valuemax="100" '
             + 'style="width: ' + percent + '%;">' + overlap + '</span></div></div>';
-        return html;
     }
 
-    function matchStack(matchSdr) {
+    function reOrderStackByOverlapScore() {
+        $stack.append($stack.children('div').detach().sort(function(left, right) {
+            var leftOverlap = parseInt($(left).find('.progress-bar').text());
+            var rightOverlap = parseInt($(right).find('.progress-bar').text());
+            return rightOverlap - leftOverlap;
+        }));
+    }
+
+    function matchStack() {
         var $nextSdr = $('#next-sdr-svg');
-        // Need to update the theta based on the matchSdr.
-        theta = Math.floor(w * (3/4));
         $stack.find('div.sdr').each(function() {
             var $sdr = $(this);
             var $meta = $sdr.find('.meta');
@@ -288,6 +306,7 @@ $(function() {
             updateUiForSdrMatch(sdr, $sdr, matchSdr);
             updateUiForSdrMatch(matchSdr, $nextSdr, sdr);
         });
+        reOrderStackByOverlapScore();
     }
 
 
@@ -301,174 +320,5 @@ $(function() {
     }
 
     initializeUi();
-
-
-    //function addNextSdr() {
-    //    $addBtn.prop('disabled', true);
-    //    $nextSdr.removeClass('highlight');
-    //    $('#next-sdr-svg').slideUp(100, function() {
-    //        $nextSdr.removeClass('highlight');
-    //        nextSdr = SDR.tools.getRandom(n, w);
-    //        drawNextSdr();
-    //        $addBtn.prop('disabled', false);
-    //    });
-    //    sdrStack.push(nextSdr);
-    //    drawSdrStack();
-    //}
-    //
-    //function calculateFalsePositiveProbability() {
-    //    return math.bignumber(0);
-    //    //return _.reduce(sdrStack, function(sum, sdr) {
-    //    //    var overlapSet = SDR.tools._getOverlapSet(n, SDR.tools.population(sdr), t, w);
-    //    //    var uniqueness = SDR.tools.getUniqueness(sdr);
-    //    //    return math.add(sum, math.divide(overlapSet, uniqueness));
-    //    //}, math.bignumber(0));
-    //}
-    //
-    //function sdrsMatch() {
-    //    return SDR.tools.population(SDR.tools.overlap(matchSdr, stackMatchSdr)) >= theta;
-    //}
-    //
-    //function updateUi() {
-    //    $nDisplay.html(n);
-    //    $wDisplay.html(w);
-    //    $tDisplay.html(t);
-    //    $thetaDisplay.html(theta);
-    //    $sparsityDisplay.html(sparsity.toFixed(2));
-    //    if (viewMode == 'add') updateUiForAdding();
-    //    else updateUiForMatching();
-    //}
-    //
-    //function updateUiForAdding() {
-    //    $wSlider.slider('option', 'disabled', false);
-    //    $wSlider.slider('option', 'max', n);
-    //    $wSlider.slider('value', w);
-    //    $falsePosDisplay.html();
-    //
-    //    $thetaSlider.slider('option', 'disabled', true);
-    //
-    //    $addBtn.prop('disabled', false);
-    //}
-    //
-    //function updateUiForMatching() {
-    //    var matchW = SDR.tools.population(matchSdr);
-    //
-    //    $falsePosDisplay.html(
-    //        calculateFalsePositiveProbability().toPrecision(5)
-    //    );
-    //
-    //    $wSlider.slider('option', 'disabled', true);
-    //
-    //    $thetaSlider.slider('option', 'disabled', false);
-    //    $thetaSlider.slider('option', 'max', matchW);
-    //    $thetaSlider.slider('value', theta);
-    //
-    //    $tSlider.slider('option', 'disabled', false);
-    //    $tSlider.slider('option', 'max', matchW - theta);
-    //    $tSlider.slider('value', t);
-    //
-    //    $addBtn.prop('disabled', true);
-    //
-    //    if (sdrsMatch()) {
-    //        $match.html('MATCH').removeClass('bg-danger').addClass('bg-success');
-    //    } else {
-    //        $match.html('NOPE').removeClass('bg-success').addClass('bg-danger');
-    //    }
-    //    $match.slideDown();
-    //
-    //    SDR.drawComparison(stackMatchSdr, matchSdr, 'match-compare', {
-    //        spartan: 'min',
-    //        size: 12
-    //    });
-    //}
-    //
-    //function drawAdd(myW, callback) {
-    //    var err = undefined;
-    //    if (validate(myW, theta, t)) {
-    //        w = myW;
-    //        sparsity = w / n;
-    //        nextSdr = SDR.tools.getRandom(n, w);
-    //        drawNextSdr();
-    //        updateUi();
-    //    } else {
-    //        err = new Error('Invalid values.');
-    //    }
-    //    if (callback) callback(err);
-    //}
-    //
-    //function drawMatch(myTheta, myT, callback) {
-    //    var err = undefined;
-    //    if (validate(w, myTheta, myT)) {
-    //        theta = myTheta;
-    //        t = myT;
-    //        matchSdr = SDR.tools.addBitNoise(stackMatchSdr, t);
-    //        drawMatchSdr();
-    //        updateUi();
-    //    } else {
-    //        err = new Error('Invalid values.');
-    //    }
-    //    if (callback) callback(err);
-    //}
-    //
-
-    //function drawSliders() {
-    //    $wSlider.slider({
-    //        min: 1, max: n, value: w, step: 1,
-    //        slide: function(event, ui) {
-    //            drawAdd(ui.value, function(err) {
-    //                if (err) event.preventDefault();
-    //            });
-    //        }
-    //    });
-    //    $thetaSlider.slider({
-    //        min: 1, max: w, value: theta, step: 1,
-    //        disabled: true,
-    //        slide: function(event, ui) {
-    //            drawMatch(ui.value, t, function(err) {
-    //                if (err) event.preventDefault();
-    //            });
-    //        }
-    //    });
-    //    $tSlider.slider({
-    //        min: 0, max: w - theta, value: t, step: 1,
-    //        disabled: true,
-    //        slide: function(event, ui) {
-    //            drawMatch(theta, ui.value, function(err) {
-    //                if (err) event.preventDefault();
-    //            });
-    //        }
-    //    });
-    //}
-    //
-    //function addButtonClickHandler() {
-    //    $addBtn.click(addNextSdr);
-    //}
-    //
-    //function addSdrClickHandler() {
-    //    $stack.click(function(evt) {
-    //        var $sdrSvg = $(evt.target).parent();
-    //        var id = $sdrSvg.attr('id');
-    //        var index = parseInt(id.split('-')[1]);
-    //        var matchW;
-    //        stackMatchSdr = sdrStack[index];
-    //        matchSdr = SDR.tools.addBitNoise(stackMatchSdr, t);
-    //        matchW = SDR.tools.population(stackMatchSdr);
-    //        if (! validate(w, theta, t, matchSdr)) {
-    //            theta = Math.floor(matchW * (3/4));
-    //            t = Math.floor((matchW - theta) * (1/2));
-    //        }
-    //        matchStack(matchSdr);
-    //        $nextSdr.addClass('highlight');
-    //        $sdrSvg.parent().addClass('highlight');
-    //        drawMatchSdr();
-    //        switchView('match');
-    //    });
-    //}
-    //
-    //drawSliders();
-    //drawNextSdr(w);
-    //addButtonClickHandler();
-    //addSdrClickHandler();
-    //switchView(viewMode);
 
 });
