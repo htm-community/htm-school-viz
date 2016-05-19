9cb7ce9c5a1e7683abb2741f67ec8760a38b2c18
Matthew Taylor <matt@numenta.org>
2016-05-19 10:39:16 -0700

    Fixed add populate stack bug in unions.

--- site/js/unions.js
+++ site/js/unions.js
@@ -175,7 +175,6 @@ $(function() {
                 sdrCount++;
             });
             drawUnion();
-            sdrStack.push(nextSdr);
             drawSdrStack();
             updateUi();
         });
