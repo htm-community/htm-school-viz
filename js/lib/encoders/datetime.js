986bafb17c4ac23d5ea195e633809e770c63b5a1
Matthew Taylor <matt@numenta.org>
2016-06-06 09:51:12 -0700

    Equalized on bits for date encodings.

--- js/lib/encoders/datetime.js
+++ js/lib/encoders/datetime.js
@@ -1,29 +1,22 @@
 $(function() {
 
+    var DEFAULT_BUCKET_WIDTH = 21;
+
     function DateEncoder() {
         this.width = 0;
         this.description = [];
         this.name = name;
 
-        //self.seasonEncoder = ScalarEncoder(w = w, minval=0, maxval=366,
-        //    radius=radius, periodic=True,
-        //    name="season", forced=forced)
-        this.seasonEncoder = new HTM.encoders.PeriodicScalarEncoder(null, 20, 91.5, 0, 366);
+        this.seasonEncoder = new HTM.encoders.PeriodicScalarEncoder(null, DEFAULT_BUCKET_WIDTH, 91.5, 0, 366);
         this.width += this.seasonEncoder.getWidth();
 
-        //self.dayOfWeekEncoder = ScalarEncoder(w = w, minval=0, maxval=7,
-        //    radius=radius, periodic=True,
-        //    name="day of week", forced=forced)
-        this.dayOfWeekEncoder = new HTM.encoders.PeriodicScalarEncoder(null, 1, 1, 0, 7);
+        this.dayOfWeekEncoder = new HTM.encoders.PeriodicScalarEncoder(null, DEFAULT_BUCKET_WIDTH, 1, 0, 7);
         this.width += this.dayOfWeekEncoder.getWidth();
 
-        //self.weekendEncoder = ScalarEncoder(w=weekend[0], minval=0, maxval=1,
-        //    periodic=False, radius=weekend[1],
-        //    name="weekend", forced=forced)
-        this.weekendEncoder = new HTM.encoders.PeriodicScalarEncoder(null, 1, 1, 0, 2);
+        this.weekendEncoder = new HTM.encoders.PeriodicScalarEncoder(null, DEFAULT_BUCKET_WIDTH, 1, 0, 2);
         this.width += this.weekendEncoder.getWidth();
 
-        this.timeOfDayEncoder = new HTM.encoders.PeriodicScalarEncoder(null, 21, 9.49, 0, 24);
+        this.timeOfDayEncoder = new HTM.encoders.PeriodicScalarEncoder(null, DEFAULT_BUCKET_WIDTH, 9.49, 0, 24);
         this.width += this.timeOfDayEncoder.getWidth();
     }
 
