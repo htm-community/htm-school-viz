5b0ade02fed68dcc13faebb1103b605575fb2501
Matthew Taylor <matt@numenta.org>
2017-07-11 10:41:00 -0700

    Episode 12 one cell per column initial implementation (#26)

--- static/js/working/tones.js
+++ static/js/working/tones.js
@@ -1,82 +1,86 @@
-var beats = 16;
-var loop;
+const beats = 16
 
-// Set up an empty sequence
-var sequence = [];
-_.times(beats, function() {
-    sequence.push([0, 0, 0, 0]);
-});
+// Set up an empty sequence.
+let sequence = []
+for (let i = 0; i < beats; i++) {
+    sequence.push([0,0,0,0])
+}
+
 //setup a polyphonic sampler
-var keys = new Tone.MultiPlayer({
+const keys = new Tone.MultiPlayer({
     urls : {
-        "A" : "./audio/casio/A1.mp3",
-        "C#" : "./audio/casio/Cs2.mp3",
-        "E" : "./audio/casio/E2.mp3",
-        "F#" : "./audio/casio/Fs2.mp3",
+        "A" : "../ep11/audio/casio/A1.mp3",
+        "C#" : "../ep11/audio/casio/Cs2.mp3",
+        "E" : "../ep11/audio/casio/E2.mp3",
+        "F#" : "../ep11/audio/casio/Fs2.mp3",
     },
     volume : -10,
     fadeOut : 0.1,
-}).toMaster();
-//the notes
-var noteNames = ["F#", "E", "C#", "A"];
+}).toMaster()
 
-function countIntsIntoArray(size) {
-    var out = [];
-    _.times(size, function(count) {
-        out.push(count);
-    });
-    return out;
+const noteNames = ["F#", "E", "C#", "A"]
+
+let countIntsIntoArray = (size) => {
+    let out = []
+    for (let i = 0; i < size; i++) {
+        out.push(i)
+    }
+    return out
 }
 
-function renderSequencerGrid(selector, beats, pads) {
-    var $grid = $(selector);
-    var $table = $('<table>');
-    _.times(pads, function(pad) {
-        var $row = $('<tr>');
-        _.times(beats, function(beat) {
-            var on = '';
-            var $cell = $('<td>');
+let renderSequencerGrid = (selector, beats, pads) => {
+    let $grid = $(selector)
+    let $table = $('<table>')
+
+    for (let pad = 0; pad < pads; pad++) {
+        let $row = $('<tr>')
+        for (let beat = 0; beat < beats; beat++) {
+            let on = ''
+            let $cell = $('<td>')
             if (sequence[beat][pad] == 1) {
-                $cell.addClass('on');
+                $cell.addClass('on')
             }
-            $cell.data('beat', beat);
-            $cell.data('pad', pad);
-            $cell.addClass('beat-' + beat);
-            $cell.addClass('pad-' + pad);
-            $row.append($cell);
-        });
-        $table.append($row);
-    });
-    $table.click(function(event) {
-        var $cell = $(event.target);
-        var beat = $cell.data('beat');
-        var pad = $cell.data('pad');
-        if (sequence[beat][pad] == 0) sequence[beat][pad] = 1;
-        else sequence[beat][pad] = 0;
-        $cell.toggleClass('on');
-    });
-    $grid.append($table);
-    return $grid;
+            $cell.data('beat', beat)
+            $cell.data('pad', pad)
+            $cell.addClass('beat-' + beat)
+            $cell.addClass('pad-' + pad)
+            $row.append($cell)
+        }
+        $table.append($row)
+    }
+
+    $table.click((event) => {
+        let $cell = $(event.target)
+        let beat = $cell.data('beat')
+        let pad = $cell.data('pad')
+        if (beat != undefined && pad != undefined) {
+            if (sequence[beat][pad] == 0) sequence[beat][pad] = 1
+            else sequence[beat][pad] = 0
+            $cell.toggleClass('on')
+        }
+    })
+
+    $grid.append($table)
+    return $grid
 }
 
 
 // Set up the SequencerInterface.
-var grid = renderSequencerGrid('#sequencer-grid', beats, 4);
+var grid = renderSequencerGrid('#sequencer-grid', beats, 4)
 
-loop = new Tone.Sequence(function(time, beat) {
-    var column = sequence[beat];
-    for (var i = 0; i < 4; i++){
-        if (column[i] === 1){
+let loop = new Tone.Sequence((time, beat) => {
+    let column = sequence[beat]
+    for (let i = 0; i < 4; i++){
+        if (column[i] === 1) {
             //slightly randomized velocities
-            var vel = Math.random() * 0.5 + 0.5;
-            keys.start(noteNames[i], time, 0, "32n", 0, vel);
+            var vel = Math.random() * 0.5 + 0.5
+            keys.start(noteNames[i], time, 0, "32n", 0, vel)
         }
     }
-    grid.find('td').removeClass('on-beat');
-    grid.find('.beat-' + beat).addClass('on-beat');
-}, countIntsIntoArray(beats), beats + "n");
+    grid.find('td').removeClass('on-beat')
+    grid.find('.beat-' + beat).addClass('on-beat')
+}, countIntsIntoArray(beats), beats + "n")
 
-Tone.Transport.start();
+Tone.Transport.start()
 
-loop.start();
-
+loop.start()
