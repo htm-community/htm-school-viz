3d0f6bd68e221a8f27117cf3cd262cf7495fc1b8
Matthew Taylor <matt@numenta.org>
2018-01-31 13:27:46 -0800

    Removed square and random GCMs

--- static/js/ep14/grid-cells.js
+++ static/js/ep14/grid-cells.js
@@ -1,8 +1,6 @@
 $(function () {
     // let GridCellModule = window.HTM.utils.gridCells.GridCellModule;
     let HexagonGridCellModule = window.HTM.gridCells.HexagonGridCellModule
-    let SquareGridCellModule = window.HTM.gridCells.SquareGridCellModule
-    let RandomGridCellModule = window.HTM.gridCells.RandomGridCellModule
     let GridCellModuleRenderer = window.HTM.gridCells.GridCellModuleRenderer
 
     let minSpacing = 60,
@@ -56,31 +54,6 @@ $(function () {
 
         modules.forEach(function(module, i) {
             let folder = gui.addFolder('Module ' + module.id)
-            let type = {type: module.type}
-            folder.add(type, 'type', ['square', 'hex', 'random']).onChange(function(value) {
-                let replacementModule;
-                if (value == 'square') {
-                    replacementModule = new SquareGridCellModule(
-                        module.id, module.xDim, module.yDim,
-                        module.orientation, module.spacing
-                    )
-                } else if (value == 'hex') {
-                    replacementModule = new HexagonGridCellModule(
-                        module.id, module.xDim, module.yDim,
-                        module.orientation, module.spacing
-                    )
-                } else if (value == 'random') {
-                    replacementModule = new RandomGridCellModule(module.id, 100, 20)
-                    let r = getRandomInt(minRgb, minRgb)
-                    let g = getRandomInt(minRgb, minRgb)
-                    let b = getRandomInt(minRgb, minRgb)
-                    replacementModule.setColor(r, g, b)
-                }
-                modules[i] = replacementModule
-                renderer.render(config.lite)
-                gui.destroy()
-                setupDatGui(modules, renderer)
-            });
             // This is because of laziness.
             module.visible = true;
             folder.add(module, 'visible').onChange(function(value) {
@@ -114,13 +87,10 @@ $(function () {
     function run() {
         prepareDom();
 
-        let GridCellModuleType = HexagonGridCellModule
-
         let numModules = 5;
         if (numModules > 5) config.lite = true;
         if (numModules == 1) {
-            let module = new GridCellModuleType(0, 3, 3, 30, 100);
-            // let module = new RandomGridCellModule(0, 100)
+            let module = new HexagonGridCellModule(0, 3, 3, 30, 100);
             gridCellModules.push(module);
         } else {
             while (gridCellModules.length < numModules) {
@@ -132,7 +102,7 @@ $(function () {
                 let r = getRandomInt(minRgb, maxRgb)
                 let g = getRandomInt(minRgb, maxRgb)
                 let b = getRandomInt(minRgb, maxRgb)
-                let module = new GridCellModuleType(id, xDim, yDim, orientation, spacing)
+                let module = new HexagonGridCellModule(id, xDim, yDim, orientation, spacing)
                 module.setColor(r, g, b)
                 gridCellModules.push(module)
             }
